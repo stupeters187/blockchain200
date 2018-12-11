0d329e3841d1597789a1ad850b8f68eee441460d
Stu Peters <stu_peters187@hotmail.com>
2018-12-11 12:20:39 -0500

    generateBlock func

--- main.go
+++ main.go
@@ -35,4 +35,15 @@ func calculateHash(block Block) string {
 
 func generateBlock(oldBlock Block, BPM int) (Block, error) {
 
+	var newBlock Block
+
+	t := time.Now()
+
+	newBlock.Index = oldBlock.Index + 1
+	newBlock.Timestamp = t.String()
+	newBlock.BPM = BPM
+	newBlock.PrevHash = oldBlock.Hash
+	newBlock.Hash = calculateHash(newBlock)
+
+	return newBlock, nil
 }
